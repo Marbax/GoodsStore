793f9861af7b66394ee8c505c7c5f254b27205ea
Marbax <40849771+Marbax@users.noreply.github.com>
2020-11-24 11:33:01 +0200

    updated bootstrap libs and models namespaces

---
+++ GoodsStore/GoodsStore.WebServer/Controllers/api/CategoryController.cs
@@ -0,0 +1,116 @@
+﻿using GoodsStore.Business.Models.Concrete;
+using GoodsStore.Business.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Transactions;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace GoodsStore.WebServer.Controllers.api
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CategoryController : ApiController
+    {
+        private readonly IServicesUnitOfWork _uow;
+
+        public CategoryController(IServicesUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var res = _uow.Categories.GetAll();
+
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var res = _uow.Categories.Get(id);
+
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create([FromBody] CategoryDTO cat)
+        {
+            try
+            {
+                CategoryDTO added = null;
+                using (var trans = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    added = _uow.Categories.Add(cat);// "add" method made save
+
+                    trans.Complete();
+                }
+                return Ok(added);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update([FromBody] CategoryDTO cat)
+        {
+            try
+            {
+                CategoryDTO updated = null;
+                using (var trans = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    _uow.Categories.CreateOrUpdate(cat);
+                    _uow.Save();
+                    updated = _uow.Categories.Get(cat.Id);
+
+                    trans.Complete();
+                }
+                return Ok(updated);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                CategoryDTO deleted = null;
+                using (var trans = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    deleted = _uow.Categories.Delete(id);
+                    _uow.Save();
+
+                    trans.Complete();
+                }
+                return Ok(deleted);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ GoodsStore/GoodsStore.WebServer/Controllers/api/UserController.cs
@@ -0,0 +1,39 @@
+﻿using GoodsStore.Business.Models;
+using GoodsStore.Business.Models.Concrete;
+using GoodsStore.Business.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Mvc;
+
+namespace GoodsStore.WebServer.Controllers.api
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UserController : ApiController
+    {
+        private readonly IServicesUnitOfWork _uow;
+
+        public UserController(IServicesUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Http.Route("login")]
+        public dynamic Login(UserDTO user)
+        {
+            if (!ModelState.IsValid)
+                return new { Code = "401", Message = "Invalid model" };
+
+            //var user = _uow.Users.Get(i => i.Name == user.Name && i.Password == user.Password).FirstOrDefault();
+
+            return new { Name = "User", Token = "someusertoken" };
+        }
+
+    }
+}
