07485996e5988a4f1b0d667bebcfcd7aea194d2d
Marbax <40849771+Marbax@users.noreply.github.com>
2020-11-26 18:53:22 +0200

    added some basic login/register pages

---
+++ GoodsStore/GoodsStore.Client.ViewModels/Concrete/UserVM.cs
@@ -0,0 +1,41 @@
+﻿using GoodsStore.Business.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodsStore.Client.ViewModels.Concrete
+{
+    public class UserVM
+    {
+        public UserDTO User { get; set; } = new UserDTO();
+        public bool IsSignedIn { get; set; } = false;
+        public string PasswordConfirm { get; set; } = "";
+        public string Message { get; set; } = "";
+        public async Task<bool> Register()
+        {
+            if (User.Password != PasswordConfirm)
+            {
+                Message = "Passwords aren't match.";
+                return false;
+            }
+
+            await Task.Delay(1000);
+            IsSignedIn = true;
+            return true;
+        }
+
+        public async Task<bool> Login()
+        {
+            await Task.Delay(1000);
+            IsSignedIn = true;
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            await Task.Delay(1000);
+            IsSignedIn = false;
+        }
+    }
+}
--- GoodsStore/GoodsStore.Client/Program.cs
+++ GoodsStore/GoodsStore.Client/Program.cs
@@ -23,6 +23,8 @@ namespace GoodsStore.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddSingleton<UserVM>();
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddHttpClient<IGenericCollectionVM<CategoryDTO>, GenericCollectionVM<CategoryDTO>>("BaseHttpClient",
