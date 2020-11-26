af2e41625e702105cc112fca5e919a637b97e141
Marbax <40849771+Marbax@users.noreply.github.com>
2020-11-26 18:50:46 +0200

    added some props for tests

--- GoodsStore/GoodsStore.Business.Models/Concrete/UserDTO.cs
+++ GoodsStore/GoodsStore.Business.Models/Concrete/UserDTO.cs
@@ -1,8 +1,17 @@
-﻿namespace GoodsStore.Business.Models.Concrete
+﻿using System.Collections.Generic;
+
+namespace GoodsStore.Business.Models.Concrete
 {
     public class UserDTO : GenericDTO
     {
+        public new string Title { get => Email; set => Email = value; }
 
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+
+        public IEnumerable<string> Roles { get; set; }
     }
-
 }
