f8472a59e57c0d0cf1453472fb7364df5b1eea03
Marbax <40849771+Marbax@users.noreply.github.com>
2021-01-14 16:05:01 +0200

    added method to find controller by dto name

--- GoodsStore/GoodsStore.Client/AppSettings.cs
+++ GoodsStore/GoodsStore.Client/AppSettings.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using GoodsStore.Business.Models.Concrete;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System;
 
 namespace GoodsStore.Client
@@ -10,6 +11,7 @@ namespace GoodsStore.Client
         public Uri RegisterUri { get; set; }
         public Uri UpdateUri { get; set; }
         public Uri CategoriesController { get; set; }
+        public Uri userController { get; set; }
 
         public AppSettings(WebAssemblyHostConfiguration config)
         {
@@ -18,6 +20,17 @@ namespace GoodsStore.Client
             RegisterUri = new Uri(BaseUri + config["registerMethod"]);
             UpdateUri = new Uri(BaseUri + config["updateProfileMethod"]);
             CategoriesController = new Uri(BaseUri + config["categoriesController"]);
+            userController = new Uri(BaseUri + config["usersController"]);
+        }
+
+        public Uri GetController(string dtoName)
+        {
+            if (dtoName == typeof(CategoryDTO).Name)
+                return CategoriesController;
+            else if (dtoName == typeof(UserDTO).Name)
+                return userController;
+            else
+                throw new ApplicationException($"Ther are no controller for {dtoName}.");
         }
 
     }
