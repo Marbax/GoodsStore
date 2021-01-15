89efdac99846acdf238e736995bc7d590b1a1667
Marbax <40849771+Marbax@users.noreply.github.com>
2021-01-15 15:05:51 +0200

    fixed autoMapper's context loosing

--- GoodsStore/GoodsStore.Domain/Concrete/UserRepo.cs
+++ GoodsStore/GoodsStore.Domain/Concrete/UserRepo.cs
@@ -1,9 +1,13 @@
 ﻿using GoodsStore.Domain.Entities;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 
 namespace GoodsStore.Domain.Concrete
 {
+    /// <summary>
+    /// AutoMapper loose context , so , thats why this exists
+    /// </summary>
     public class UserRepo : GenericRepository<User>
     {
         private DbSet<Role> _roles;
@@ -19,6 +23,15 @@ namespace GoodsStore.Domain.Concrete
             var roles = _roles.Where(i => rIds.Contains(i.Id)).ToList();
             entity.Roles = roles;
             return _dbSet.Add(entity);
+        }
+
+        public override void CreateOrUpdate(User entity)
+        {
+            var contextEntity = Get(entity.Id);
+            var rIds = entity.Roles.Select(i => i.Id).ToList();
+            var roles = _roles.Where(i => rIds.Contains(i.Id)).ToList();
+            contextEntity.Roles = roles;
+            _dbSet.AddOrUpdate(entity);
         }
     }
 }
